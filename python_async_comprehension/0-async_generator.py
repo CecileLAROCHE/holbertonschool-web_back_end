2fdcf4914d212b46ea7622a8ed6008ec6fc861c2
CecileLAROCHE <11458@holbertonstudents.com>
2025-11-27 10:13:27 +0100

    task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+
+"""
+import asyncio
+import random
+from typing import List, AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Génère 10 floats entre 0 et 10, avec 1 sec entre chaque.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
+    

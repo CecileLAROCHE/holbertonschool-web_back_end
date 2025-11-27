f3346948b07c4b6205eb2a54a38d5f8b7c4bedb4
CecileLAROCHE <11458@holbertonstudents.com>
2025-11-27 11:28:51 +0100

    ajout des commentaires

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-
+Module contenant une coroutine permettant de mesurer le temps
+d'exécution de plusieurs compréhensions asynchrones lancées en parallèle.
 """
 
 import time
@@ -11,7 +12,18 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """
-    ***
+    Mesure le temps nécessaire pour exécuter quatre appels
+    à async_comprehension en parallèle.
+
+    Cette fonction :
+        - démarre un chronomètre,
+        - lance quatre compréhensions asynchrones simultanément à l'aide de
+          asyncio.gather(), ce qui permet leur exécution parallèle,
+        - attend que toutes soient terminées,
+        - calcule et renvoie le temps total écoulé.
+
+    Retour :
+        float : le temps total d'exécution en secondes.
     """
     start = time.time()
 

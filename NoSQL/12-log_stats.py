8fe3b8c251979c7fb965e7e1a4498d71b671ef6e
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-12-04 00:23:56 +0100

    corrections

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
 """
-Module qui contient une seule fonction : schools_by_topic
+Script that provides some stats about Nginx logs stored in MongoDB.
 """
 
 from pymongo import MongoClient
-client = MongoClient()
-db = client.logs
-collection = db.nginx
-nb_get = collection.count_documents({"method": "GET"})
-nb_post = collection.count_documents({"method": "POST"})
-nb_put = collection.count_documents({"method": "PUT"})
-nb_patch = collection.count_documents({"method": "PATCH"})
-nb_delete = collection.count_documents({"method": "DELETE"})
-nb_status = collection.count_documents({"method": "GET", "path": "/status"})
-total_logs = collection.count_documents({})
-print(f"{total_logs} logs")
-print("Methods:")
-print(f"\tmethod GET: {nb_get}")
-print(f"\tmethod POST: {nb_post}")
-print(f"\tmethod PUT: {nb_put}")
-print(f"\tmethod PATCH: {nb_patch}")
-print(f"\tmethod DELETE: {nb_delete}")
-print(f"{nb_status} status check")
+
+if __name__ == "__main__":
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
+
+    total_logs = collection.count_documents({})
+
+    print(f"{total_logs} logs")
+    print("Methods:")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_check = collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+    print(f"{status_check} status check")

9d5cdf28b963008bc317479cb81d6cc7ffdb4337
Cecile.LAROCHE <11458@holbertonstudents.com>
2025-12-12 14:50:51 +0100

    correction

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,20 +2,21 @@ export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
 
-    // Vérifie si la méthode evacuationWarningMessage a été redéfinie
-    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+    // Vérifie si on n'est PAS dans la classe Building elle-même
+    // (donc si on est dans une classe enfant)
+    if (this.constructor !== Building &&
+        this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
       throw new Error("Class extending Building must override evacuationWarningMessage");
     }
   }
 
-  // ----- getter -----
+  // getter
   get sqft() {
     return this._sqft;
   }
 
-  // ----- méthode abstraite -----
+  // méthode "abstraite"
   evacuationWarningMessage() {
-    // Méthode vide dans le parent
-    // Toute classe enfant doit la redéfinir
+    // Doit être overriden par les enfants
   }
 }

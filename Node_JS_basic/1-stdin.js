7b381deb184ccccf6f84a7455eb82f4474a1477a
CecileLAROCHE <11458@holbertonstudents.com>
2026-01-05 16:00:57 +0100

    task 1 en cours

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+console.log("Welcome to Holberton School, what is your name?")
+
+process.stdin.on('data', (input) => {
+      // affiche "Your name is: INPUT"
+})
+
+process.stdin.on(() => {
+      console.log("This important software is now closing")
+
+})
+

39464fae60ff06d6fd7a3beb9e6384bb7a6b8f1f
CecileLAROCHE <11458@holbertonstudents.com>
2026-01-06 11:24:22 +0100

    task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,9 @@
-console.log("Welcome to Holberton School, what is your name?")
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) process.stdout.write(`Your name is: ${name}`);
+});
 
-process.stdin.on('data', (input) => {
-      // affiche "Your name is: INPUT"
-})
-
-process.stdin.on(() => {
-      console.log("This important software is now closing")
-
-})
-
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
+});

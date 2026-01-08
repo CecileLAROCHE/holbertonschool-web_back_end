980d938d0f1e57691c19df795b4387e0d0d0dd63
CecileLAROCHE <11458@holbertonstudents.com>
2026-01-08 11:07:31 +0100

    task 4 -> mise en forme

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,15 +2,13 @@ const { createServer } = require('node:http');
 
 const port = 1245;
 
-
 const app = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-module.exports = app
-
+module.exports = app;
 
 app.listen(port, () => {
   console.log(`app running at ${port}/`);
